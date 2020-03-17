cb1940d069c4d493c6ff41f262d19c72bf93b458
김홍규 <>
2020-03-17 10:00:53 +0900

    jsp 관련 설정 파일 추가 및 jsp 테스트 코드 추가

--- src/main/java/com/mih/springpractice/WelcomeController.java
+++ src/main/java/com/mih/springpractice/WelcomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
   @RequestMapping("/welcome")
   public String welcome() {
+    System.out.println("welcome!!!!");
     return "welcome";
   }
 }
